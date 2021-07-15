bf37fe9e4f17f5d282c659132344390358e6207f
Rodrigo Mendes <35048303+rodimendes@users.noreply.github.com>
2021-07-15 21:32:05 +0000

    Palavra em lista, chances

--- main.py
+++ main.py
@@ -18,9 +18,27 @@ jogadores = nomes(quant_jog)
 
 #Início do jogo
 lista_palavras = []
-for jogador in jogadores:
-    palavra = input(f"{jogador}, é a sua vez de inserir uma palavra com a letra '{letra.upper()}': ").capitalize()
-    lista_palavras.append(palavra)
+jogo = True
+rodada = 1
+while jogo:
+    print("=-"*20)
+    print(f"\n               Rodada {rodada}\n")
+    print("=-"*20)
+    for jogador in jogadores:
+        palavra = input(f"{jogador}, é a sua vez de inserir uma palavra com a letra '{letra.upper()}': [Pressione '0' para sair.").capitalize()
+        if palavra == "0":
+            jogo = False
+        while palavra in lista_palavras:
+            chance = 3
+            palavra = input(f"{jogador}, esta palavra já foi escolhida. Pense em outra. Você só tem {chance} chances de inserir uma palavra com a letra '{letra.upper()}': ").capitalize()
+            chance -= 1
+            if chance == 0:
+                print('Você perdeu.\nFim do jogo!')
+                jogo = False
+        lista_palavras.append(palavra)
+    rodada += 1
 print(lista_palavras)
 
-# uma palavra por rodada por participante
+#resolver problema do zero aparecer na lista
+#resolver problema do zero nao parar imediatamente
+#quando escrever com letra errada, avisar e pedir para repetir
