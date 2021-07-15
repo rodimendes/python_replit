aeb049390901c449c711595460d8395c796da88d
Rodrigo Mendes <35048303+rodimendes@users.noreply.github.com>
2021-07-15 13:33:16 +0000

    Incluido letra e lista de palavras

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 def nomes(quant_jog):
     participantes = []
     for x in range(quant_jog):
-        participantes.append(input(f"Digite o nome do {x+1}° participante: "))
+        nome = input(f"Digite o nome do {x+1}° participante: ").capitalize()
+        participantes.append(nome)
     print(f"OK! {participantes} irão jogar.")
     return participantes
 
-def lista(palavra):
-    lista_palavras = []
-    
-num_jogadores = int(input("Quantos jogadores participarão? "))
+#==============================#   
+#Define a quantidade de jogadores
+quant_jog = int(input("Quantos jogadores participarão? "))
 
-jogadores = nomes(num_jogadores)
+#Define a letra da rodada
+letra = input("Qual a letra desta rodada? ").lower()
 
+#Aciona a função para nomear os jogadores
+jogadores = nomes(quant_jog)
+
+#Início do jogo
+lista_palavras = []
+for jogador in jogadores:
+    palavra = input(f"{jogador}, é a sua vez de inserir uma palavra com a letra '{letra.upper()}': ").capitalize()
+    lista_palavras.append(palavra)
+print(lista_palavras)
 
 # uma palavra por rodada por participante
