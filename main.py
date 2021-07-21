e4f76458ef9f9ee3c0eac64aa132a9ea3cbbbe05
Rodrigo Mendes <35048303+rodimendes@users.noreply.github.com>
2021-07-21 22:14:05 +0000

    Teste finalizar jogo

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def nomes(quant_jog):
 quant_jog = int(input("Quantos jogadores participarão? "))
 
 #Define a letra da rodada
-letra = input("Qual a letra desta rodada? ").lower()
+letra = input("Qual a letra desta rodada? ")[0].lower()
 
 #Aciona a função para nomear os jogadores
 jogadores = nomes(quant_jog)
@@ -20,21 +20,24 @@ jogadores = nomes(quant_jog)
 lista_palavras = []
 jogo = True
 rodada = 1
-while jogo:
+while jogo == True:
     print("=-"*20)
     print(f"\n               Rodada {rodada}\n")
     print("=-"*20)
     for jogador in jogadores:
-        palavra = input(f"{jogador}, é a sua vez de inserir uma palavra com a letra '{letra.upper()}': [Pressione '0' para sair.").capitalize()
+        palavra = input(f"{jogador}, é a sua vez de inserir uma palavra com a letra '{letra.upper()}': [Pressione '0' para sair.]\n").lower()
         if palavra == "0":
             jogo = False
         while palavra in lista_palavras:
             chance = 3
-            palavra = input(f"{jogador}, esta palavra já foi escolhida. Pense em outra. Você só tem {chance} chances de inserir uma palavra com a letra '{letra.upper()}': ").capitalize()
+            palavra = input(f"{jogador}, esta palavra já foi escolhida. Pense em outra. Você só tem {chance} chances de inserir uma palavra com a letra '{letra.upper()}':\n").capitalize()
             chance -= 1
             if chance == 0:
                 print('Você perdeu.\nFim do jogo!')
-                jogo = False
+                jogo == False
+        while palavra[0] != letra:
+            palavra = input("A palavra não inicia com a letra escolhida. Tente outra:\n")
+            
         lista_palavras.append(palavra)
     rodada += 1
 print(lista_palavras)
